b5887e1cf7d59e2a01166a43f2fdd4da1d37a758
NazarLutsyk <nlutsik1@gmail.com>
2018-05-10 18:55:28 +0300

    nazik init

--- src/configs/GlobalVariables.ts
+++ src/configs/GlobalVariables.ts
@@ -1,6 +1,6 @@
-// export const host1 = 'https://localhost:3000';
-// export const host2 = 'https://localhost:3000';
-export const host1 = 'https://192.168.1.25:3000';
-export const host2 = 'https://192.168.1.25:3000';
+export const host1 = 'https://localhost:3000';
+export const host2 = 'https://localhost:3000';
+// export const host1 = 'https://192.168.1.25:3000';
+// export const host2 = 'https://192.168.1.25:3000';
 // export const host2 = 'https://127.0.0.1:3000';
 export const lang = '5ae8746c8438842f9c41caa3';
