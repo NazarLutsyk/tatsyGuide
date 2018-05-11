0dbc6359b0201ca7f5dfa4d7c43a211eb3d6b9dc
NazarLutsyk <nlutsik1@gmail.com>
2018-05-11 19:28:00 +0300

    rest

---
+++ src/providers/http-interceptor/http-interceptor.ts
@@ -0,0 +1,19 @@
+import {HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+
+@Injectable()
+export class HttpInterceptorProvider implements HttpInterceptor{
+
+  constructor(private http: HttpClient) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let updatedRes = req.clone({
+      withCredentials: true
+    });
+    return next.handle(updatedRes);
+  }
+
+
+}
