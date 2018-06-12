d2a0f8981fffc55c271c36811eb098e912507204
sergeylogos <sergey.logos@gmail.com>
2018-06-12 18:00:24 +0300

    commit after merge

--- src/configs/GlobalConfigsService.ts
+++ src/configs/GlobalConfigsService.ts
@@ -1,6 +1,5 @@
 import {Injectable} from "@angular/core";
 import {Platform} from "ionic-angular";
-import {LangProvider} from "../providers/lang/lang";
 
 @Injectable()
 export class GlobalConfigsService {
@@ -23,8 +22,14 @@ export class GlobalConfigsService {
 
   getGlobalLang() {
 
-    console.log(this.globalLang);
+    if (this.globalLang.contains('ru') || this.globalLang.contains('RU') || this.globalLang.contains('ua') || this.globalLang.contains('UA')) {
+      return "5b1e7e02749f5b3aa072ccf4";
+    } else if (this.globalLang.contains('en') || this.globalLang.contains('EN')) {
+      return "5b1e7e02749f5b3aa072ccf4"
+    }
     return "5b1e7e02749f5b3aa072ccf4";
+
+
   }
 
 
